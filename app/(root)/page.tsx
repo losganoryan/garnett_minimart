20ac10600accd5606e09ef0e00d6826472bcc728
ryan_losgano <ryan.losgano@transcom.com>
2025-03-04 23:05:53 +0800

    Worked with Database, prisma and Product display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,11 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const Homepage = () => {
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Latest Products"
-        limit={4}
-      />
+      <ProductList data={latestProducts} title="Latest Products" />
     </>
   );
 };
